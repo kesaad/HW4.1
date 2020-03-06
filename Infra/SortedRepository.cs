f35766114c3e77a619fdfebf775f4b5bed520587
kesaad <kesaad@ttu.ee>
2020-03-06 21:18:07 +0200

    P'rast 23.videot

--- Infra/SortedRepository.cs
+++ Infra/SortedRepository.cs
@@ -5,13 +5,6 @@ using System.Reflection;
 using Abc.Data.Common;
 using Abc.Domain;
 using Microsoft.EntityFrameworkCore;
-//TODO
-//case "ValidFrom":
-//measures = measures.OrderBy(s => s.ValidFrom);
-//break;
-//case "ValidFrom_desc":
-//measures = measures.OrderByDescending(s => s.ValidFrom);
-//break;
 
 namespace Abc.Infra
 {
@@ -26,15 +19,14 @@ namespace Abc.Infra
         protected internal IQueryable<TData> setSorting(IQueryable<TData> data)
         {
             var expression = createExpression();
-            if (expression is null) return data;
-            return setOrderBy(data, expression);
+            var r =  expression is null ? data : setOrderBy(data, expression);
+            return r;
         }
 
         internal Expression<Func<TData, object>> createExpression()
         {
             var property = findProperty();
-            if (property is null) return null;
-            return lambdaExpression(property);
+            return property is null ? null : lambdaExpression(property);
         }
 
         internal Expression<Func<TData, object>> lambdaExpression(PropertyInfo p)
--- Tests/Infra/SortedRepositoryTests.cs
+++ Tests/Infra/SortedRepositoryTests.cs
@@ -21,9 +21,10 @@ namespace Abc.Tests.Infra
         {
             public testClass(DbContext c, DbSet<MeasureData> s) : base(c, s) { }
 
-            protected override Task<MeasureData> getData(string id)
+            protected override async Task<MeasureData> getData(string id)
             {
-                throw new NotImplementedException();
+                await Task.CompletedTask;
+                return new MeasureData();
             }
         }
 
@@ -31,7 +32,8 @@ namespace Abc.Tests.Infra
         public override void TestInitialize()
         {
             base.TestInitialize();
-            var c = new QuantityDbContext(new DbContextOptions<QuantityDbContext>());
+            var options = new DbContextOptionsBuilder<QuantityDbContext>().UseInMemoryDatabase("TestDb").Options;
+            var c = new QuantityDbContext(options);
             obj = new testClass(c, c.Measures);
         }
 
@@ -51,7 +53,29 @@ namespace Abc.Tests.Infra
         [TestMethod]
         public void SetSortingTest()
         {
-            Assert.Inconclusive();
+            void test(IQueryable<MeasureData> d, string sortOrder)
+            {
+                obj.SortOrder = sortOrder + obj.DescendingString;
+                var set = obj.setSorting(d);
+                Assert.IsNotNull(set);
+                Assert.AreNotEqual(d, set);
+                Assert.IsTrue(set.Expression.ToString().Contains($"Abc.Data.Quantity.MeasureData]).OrderByDescending(Param_0 => Convert(Param_0.{sortOrder}, Object))"));
+                obj.SortOrder = sortOrder;
+                set = obj.setSorting(d);
+                Assert.IsNotNull(set);
+                Assert.AreNotEqual(d, set);
+                Assert.IsTrue(set.Expression.ToString().Contains($"Abc.Data.Quantity.MeasureData]).OrderBy(Param_0 => Convert(Param_0.{sortOrder}, Object))"));
+            }
+            Assert.IsNull(obj.setSorting(null));
+            IQueryable<MeasureData> data = obj.dbSet;
+            obj.SortOrder = null;
+            Assert.AreEqual(data, obj.setSorting(data));
+            test(data, GetMember.Name<MeasureData>(x => x.Id));
+            test(data, GetMember.Name<MeasureData>(x => x.Code));
+            test(data, GetMember.Name<MeasureData>(x => x.Name));
+            test(data, GetMember.Name<MeasureData>(x => x.Definition));
+            test(data, GetMember.Name<MeasureData>(x => x.ValidFrom));
+            test(data, GetMember.Name<MeasureData>(x => x.ValidTo));
         }
 
         [TestMethod]
@@ -151,13 +175,28 @@ namespace Abc.Tests.Infra
         [TestMethod]
         public void SetOrderByTest()
         {
-            Expression<Func<MeasureData, object>> expression = measureData => measureData.ToString();
+            void test(IQueryable<MeasureData> d, Expression<Func<MeasureData, object>> e, string expected)
+            {
+                obj.SortOrder = GetRandom.String() + obj.DescendingString;
+                var set = obj.setOrderBy(d, e);
+                Assert.IsNotNull(set);
+                Assert.AreNotEqual(d, set);
+                Assert.IsTrue(set.Expression.ToString().Contains($"Abc.Data.Quantity.MeasureData]).OrderByDescending({expected}"));
+                obj.SortOrder = GetRandom.String();
+                set = obj.setOrderBy(d, e);
+                Assert.IsNotNull(set);
+                Assert.AreNotEqual(d, set);
+                Assert.IsTrue(set.Expression.ToString().Contains($"Abc.Data.Quantity.MeasureData]).OrderBy({expected})"));
+            }
             Assert.IsNull(obj.setOrderBy(null, null));
             IQueryable<MeasureData> data = obj.dbSet;
             Assert.AreEqual(data, obj.setOrderBy(data, null));
-            Assert.AreEqual(data, obj.setOrderBy(data, expression));
-            var data1 = obj.setOrderBy(data, x => x.Id);
-            Assert.AreEqual(data, data1);
+            test(data, x => x.Id, "x => x.Id");
+            test(data, x => x.Code, "x => x.Code");
+            test(data, x => x.Name, "x => x.Name");
+            test(data, x => x.Definition, "x => x.Definition");
+            test(data, x => x.ValidFrom, "x => Convert(x.ValidFrom, Object)");
+            test(data, x => x.ValidTo, "x => Convert(x.ValidTo, Object)");
         }
 
         [TestMethod]
