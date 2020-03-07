8e9323d10c8355a9cd4834109e50d5fa5aa5a615
kesaad <kesaad@ttu.ee>
2020-03-07 18:41:29 +0200

    P'rast 26.videot

--- Infra/Quantity/MeasuresRepository.cs
+++ Infra/Quantity/MeasuresRepository.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq;
-using Abc.Data.Quantity;
+﻿using Abc.Data.Quantity;
 using Abc.Domain.Quantity;
+
 namespace Abc.Infra.Quantity
 {
     public class MeasuresRepository : UniqueEntityRepository<Measure, MeasureData>, IMeasuresRepository
@@ -8,17 +8,5 @@ namespace Abc.Infra.Quantity
         public MeasuresRepository(QuantityDbContext c) : base(c, c.Measures) { }
 
         protected internal override Measure toDomainObject(MeasureData d) => new Measure(d);
-
-        protected internal override IQueryable<MeasureData> addFiltering(IQueryable<MeasureData> set)
-        {
-            if (string.IsNullOrEmpty(SearchString)) return set;
-            return set.Where(s => s.Name.Contains(SearchString)
-            || s.Code.Contains(SearchString)
-            || s.Id.Contains(SearchString)
-            || s.Definition.Contains(SearchString)
-            || s.ValidFrom.ToString().Contains(SearchString)
-            || s.ValidTo.ToString().Contains(SearchString)
-            );
-        }
     }
 }
